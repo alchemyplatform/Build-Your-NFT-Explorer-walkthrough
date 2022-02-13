5432c41fcaa545b988127862a2d1309bfffec520
Kunal Roy <kunal@alchemyapi.io>
2022-02-13 08:16:58 -0800

    comments

--- src/components/nftcard.jsx
+++ src/components/nftcard.jsx
@@ -8,34 +8,34 @@ import { ClipboardIcon } from "@heroicons/react/outline"
 
 
 const NftCard = ({ image, id, title, address, description, attributes }) => {
-    // return (
-    //     <div className="w-1/4 mr-3 mb-4 bg-slate-100 rounded-md" >
-    //         <img className='w-full rounded-t-md' key={id} src={undefined}></img>
-    //         <div className="p-3">
-    //             <div className="flex mb-3">
-    //                 <div className="flex-grow">
-    //                     <h3 className="text-xl">{}</h3>
-    //                     {/*<p>{`${id.slice(0, 4)}...${id.slice(id.length - 4)}`}</p>*/}
-    //                 </div>
-    //                 <div className="flex mr-3">
-    //                     <a target="_blank" className="text-blue-700" href={`https://etherscan.io/token/${address}`}>{`${address.slice(0, 4)}...${address.slice(address.length - 4)}`}</a>
-    //                     <ClipboardIcon onClick={() => navigator.clipboard.writeText(address)} className="h-4 w-4 -mt-1 text-black cursor-pointer"></ClipboardIcon>
-    //                 </div>
-    //             </div>
-    //             <p>{description? description.slice(0, 200) : "No Description"}</p>
-    //         </div>
-    //         <div className="flex flex-wrap justify-center items-center p-3 ">
-    //             {attributes?.length > 0 && attributes.map(attribute => {
-    //                 return (
-    //                     <div className="w-1/2 mb-2 flex justify-start flex-col">
-    //                         <p className="mr-2 font-bold">{}:</p>
-    //                         <p className="text-sm">{}</p>
-    //                     </div>
-    //                 )
-    //             })}
-    //         </div>
-    //     </div>
-    // )
+    return (
+        <div className="w-1/4 mr-3 mb-4 bg-slate-100 rounded-md" >
+            <img className='w-full rounded-t-md' key={id} src={undefined}></img>
+            <div className="p-3">
+                <div className="flex mb-3">
+                    <div className="flex-grow">
+                        <h3 className="text-xl">{}</h3>
+                        {/*<p>{`${id.slice(0, 4)}...${id.slice(id.length - 4)}`}</p>*/}
+                    </div>
+                    <div className="flex mr-3">
+                        {/*<a target="_blank" className="text-blue-700" href={`https://etherscan.io/token/${address}`}>{`${address.slice(0, 4)}...${address.slice(address.length - 4)}`}</a>*/}
+                        {/*<ClipboardIcon onClick={() => navigator.clipboard.writeText(address)} className="h-4 w-4 -mt-1 text-black cursor-pointer"></ClipboardIcon>*/}
+                    </div>
+                </div>
+                {/*<p>{description? description.slice(0, 200) : "No Description"}</p>*/}
+            </div>
+            <div className="flex flex-wrap justify-center items-center p-3 ">
+                {/*{attributes?.length > 0 && attributes.map(attribute => {*/}
+                {/*    return (*/}
+                {/*        <div className="w-1/2 mb-2 flex justify-start flex-col">*/}
+                {/*            <p className="mr-2 font-bold">{}:</p>*/}
+                {/*            <p className="text-sm">{}</p>*/}
+                {/*        </div>*/}
+                {/*    )*/}
+                {/*})}*/}
+            </div>
+        </div>
+    )
 }
 
 export default NftCard
--- src/utils/fetchNFTs.js
+++ src/utils/fetchNFTs.js
@@ -1,19 +1,22 @@
 
-const getAddressNFTs = async (endpoint, owner, contractAddress) => {
+const getAddressNFTs = async (endpoint, owner, contractAddress, retryAttempt) => {
+    if (retryAttempt === 5) {
+        return;
+    }
     if (owner) {
         let data;
         try {
             if (contractAddress) {
-                data = await fetch(`${endpoint}/v1/getNFTs?owner=${owner}&contractAddresses%5B%5D=${contractAddress}`).then(data => data.json())
+                data = await fetch(`${endpoint}/getNFTs?owner=${owner}&contractAddresses%5B%5D=${contractAddress}`).then(data => data.json())
 
             } else {
                 // data = await fetch(`${endpoint}/v1/getNFTs?owner=${owner}`).then(data => data.json())
-                data = await fetch(`${endpoint}/v1/getNFTs?owner=${owner}`).then(data => data.json())
+                data = await fetch(`${endpoint}/getNFTs?owner=${owner}`).then(data => data.json())
 
             }
             // console.log("GETNFTS: ", data)
         } catch (e) {
-            getAddressNFTs(endpoint, owner, contractAddress)
+            getAddressNFTs(endpoint, owner, contractAddress, retryAttempt+1)
         }
 
         return data
@@ -39,23 +42,23 @@ const getEndpoint = (chain) => {
 
 const fetchNFTs = async (owner, setNFTs, chain, contractAddress) => {
     let endpoint = getEndpoint(chain)
-    const data = await getAddressNFTs(endpoint, owner, contractAddress)
-    if (data.ownedNfts.length) {
-        const NFTs = await getNFTsMetadata(data.ownedNfts, endpoint)
-        console.log("NFTS metadata", NFTs)
-        let fullfilledNFTs = NFTs.filter(NFT => NFT.status == "fulfilled")
-        console.log("NFTS", fullfilledNFTs)
-        setNFTs(fullfilledNFTs)
-    } else {
-        setNFTs(null)
-    }
+    // const data = await getAddressNFTs(endpoint, owner, contractAddress)
+    // if (data.ownedNfts.length) {
+    //     const NFTs = await getNFTsMetadata(data.ownedNfts, endpoint)
+    //     console.log("NFTS metadata", NFTs)
+    //     let fullfilledNFTs = NFTs.filter(NFT => NFT.status == "fulfilled")
+    //     console.log("NFTS", fullfilledNFTs)
+    //     setNFTs(fullfilledNFTs)
+    // } else {
+    //     setNFTs(null)
+    // }
 
 }
 
 
 const getNFTsMetadata = async (NFTS, endpoint) => {
     const NFTsMetadata = await Promise.allSettled(NFTS.map(async (NFT) => {
-        const metadata = await fetch(`${endpoint}/v1/getNFTMetadata?contractAddress=${NFT.contract.address}&tokenId=${NFT.id.tokenId}`,).then(data => data.json())
+        const metadata = await fetch(`${endpoint}/getNFTMetadata?contractAddress=${NFT.contract.address}&tokenId=${NFT.id.tokenId}`,).then(data => data.json())
         let image;
         console.log("metadata", metadata)
         if (metadata.media[0].uri.gateway.length) {
